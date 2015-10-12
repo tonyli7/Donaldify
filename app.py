682253022968f500351472bad0dfeba3befa7f35
Johnny So <jso123450@gmail.com>
2015-10-12 01:21:32 -0400

    Fixed button links

--- app.py
+++ app.py
@@ -1,15 +1,19 @@
+# imports
+
 from flask import Flask, render_template, request, session, redirect, url_for
 import sqlite3
 
+#flask
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     return render_template("home.html")
 
-@app.route("/login")
+@app.route("/login",methods=["GET","POST"])
 def login():
-    return render_template("login.html")
+    if request.method=="GET":
+        return render_template("login.html")
 
 @app.route("/register")
 def register():
