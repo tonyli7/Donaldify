4b1e47018ec3673a33ca7924d6c83e9b77527cbe
Johnny So <jso123450@gmail.com>
2015-10-12 22:10:42 -0400

    Added a loginauth to utils

--- utils.py
+++ utils.py
@@ -1,5 +1,10 @@
-# Utilities page
+# Utilities File
 
 # Put misc functions such as login authentication here
+def loginauth(un,pw):
+    if un=="HERES" and pw=="JOHNNY":
+        return True;
+    else:
+        return False;
 
 # Maybe something for inserting a new UN/PW combo from register too
