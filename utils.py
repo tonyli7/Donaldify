05e6625c37c413b3114c1f21afbbc02ba2f1e417
ishraqbhuiyan <ishraqbhuiyan@gmail.com>
2015-10-15 11:25:00 -0400

    move edit

--- utils.py
+++ utils.py
@@ -34,11 +34,25 @@ def getPost(title):
 
 #how to read post table, for loop, index 0 is user, 1 is title, 2 is content
 
+def edit(user, title, new_content):
+    conn = sqlite3.connect('bloginator.db')
+    c = conn.cursor()
+    q = """
+    UPDATE post
+    SET content='""" + new_content +"""'
+    WHERE user='""" + user + """'
+    AND title='""" + title + "'"
+    c.execute(q)
+    conn.commit()
+    conn.close()
 
-
-
-
-
-
-
-
+def delete(user, title):
+    conn = sqlite3.connect('bloginator.db')
+    c = conn.cursor()
+    q = """
+    DELETE FROM post
+    WHERE user='"""+ user """'
+    AND title='"""+ title + "'"
+    c.execute(q)
+    conn.commit()
+    conn.close()
